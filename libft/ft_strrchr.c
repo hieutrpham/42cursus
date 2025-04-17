f2b154e413736ad3ce56793ca0b8a8aa34dbcba0
hieutrpham <hieutrungpham91@gmail.com>
2025-04-17 17:40:26 +0300

    add strchr strrchr

---
+++ libft/ft_strrchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trupham <trupham@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/17 17:31:55 by trupham           #+#    #+#             */
+/*   Updated: 2025/04/17 17:40:03 by trupham          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	i;
+	int	len;
+
+	len = ft_strlen(s);
+	i = 0;
+	c = (char)c;
+	while (len >= 0)
+	{
+		if (s[len] == c)
+			return ((char *)&s[len]);
+		len--;
+	}
+	return (NULL);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: trupham <trupham@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 11:36:45 by trupham           #+#    #+#             */
-/*   Updated: 2025/04/17 15:41:03 by trupham          ###   ########.fr       */
+/*   Updated: 2025/04/17 17:38:46 by trupham          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,5 +30,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 int		ft_toupper(int c);
-int	ft_tolower(int c);
+int		ft_tolower(int c);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strchr(const char *s, int c);
 #endif
