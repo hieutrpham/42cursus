828a7ae667c22789943ef640785a79b2daaa210a
hieutrpham <hieutrungpham91@gmail.com>
2025-04-17 15:16:27 +0300

    strlcat

---
+++ libft/ft_strlcat.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trupham <trupham@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/17 11:35:02 by trupham           #+#    #+#             */
+/*   Updated: 2025/04/17 11:35:02 by trupham          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stddef.h>
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	len_dest;
+	size_t	len_src;
+	size_t	i;
+	size_t	j;
+
+	j = 0;
+	i = 0;
+	len_dest = ft_strlen(dst);
+	len_src = ft_strlen(src);
+	if (size <= 0 || len_dest >= size)
+		return (len_src + size);
+	while (dst[i])
+		i++;
+	while (src[j] && j < size - len_dest - 1)
+	{
+		dst[i] = src[j];
+		i++;
+		j++;
+	}
+	dst[i] = 0;
+	return (len_src + len_dest);
+}
