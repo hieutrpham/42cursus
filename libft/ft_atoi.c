f6261e0471c757d40b3c660ca634dea68c837f79
hieutrpham <hieutrungpham91@gmail.com>
2025-04-18 15:41:48 +0300

    add atoi

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -12,7 +12,37 @@
 
 #include "libft.h"
 
- int ft_atoi(const char *nptr)
+static int	isspace(int c)
 {
+	c = (char)c;
+	if (c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == '\v'
+		|| c == '\f')
+		return (1);
+	return (0);
+}
 
+int	ft_atoi(const char *nptr)
+{
+	int	i;
+	int	sign;
+	int	atoi;
+
+	atoi = 0;
+	i = 0;
+	sign = 1;
+	while (isspace(nptr[i]))
+		i++;
+	if (nptr[i] == '+')
+		i++;
+	else if (nptr[i] == '-')
+	{
+		sign = -sign;
+		i++;
+	}
+	while (nptr[i] && ft_isdigit(nptr[i]))
+	{
+		atoi = atoi * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return (atoi * sign);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -18,6 +18,7 @@
 # include <stddef.h>
 # include <stdio.h>
 # include <string.h>
+# include <stdlib.h>
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
@@ -38,4 +39,5 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *smol, size_t len);
+int		ft_atoi(const char *nptr);
 #endif
