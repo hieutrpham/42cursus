8b3fcc76096780a46ac3ccfbace249242fb6be85
hieutrpham <hieutrungpham91@gmail.com>
2025-04-18 11:17:24 +0300

    update strncmp

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: trupham <trupham@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 09:54:30 by trupham           #+#    #+#             */
-/*   Updated: 2025/04/18 11:13:58 by trupham          ###   ########.fr       */
+/*   Updated: 2025/04/18 11:17:10 by trupham          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	int	sum;
 
 	sum = 0;
-	if (n <= 0)
-		return (0);
 	while (n && (*s1 || *s2))
 	{
 		sum = *s1 - *s2;
 		if (sum != 0)
-			return (sum);
+			break ;
 		n--;
 		s1++;
 		s2++;
