c7d0fd9d722097680d14fa08eaeaa33ac1b7a4e9
hieutrpham <hieutrungpham91@gmail.com>
2025-04-30 16:56:35 +0300

    get next line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,11 +6,22 @@
 /*   By: trupham <trupham@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 11:56:11 by trupham           #+#    #+#             */
-/*   Updated: 2025/04/30 13:34:25 by trupham          ###   ########.fr       */
+/*   Updated: 2025/04/30 16:50:12 by trupham          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "get_next_line.h"
 
-char *get_next_line(int fd)
+char *get_next_line(int fd);
+
+int main()
 {
+	char str[1];
+	size_t t;
+	int fd = open("hello.txt", O_RDONLY);
 
+	while (read(fd, str, 5))
+	{
+		printf("%s\n", str);
+	}
+	return 0;
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,12 +6,17 @@
 /*   By: trupham <trupham@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 13:52:28 by trupham           #+#    #+#             */
-/*   Updated: 2025/04/30 13:56:38 by trupham          ###   ########.fr       */
+/*   Updated: 2025/04/30 16:30:25 by trupham          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#include <stdlib.h>
+#define GET_NEXT_LINE_H
 
+# include <fcntl.h>
+# include <stddef.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
 char *get_next_line(int fd);
 #endif // !GET_NEXT_LINE_H
