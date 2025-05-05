23ad7ba37edbecb42625134eeecb44a0f477a159
hieutrpham <hieutrungpham91@gmail.com>
2025-05-05 17:45:58 +0300

    update

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -10,37 +10,83 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
+#include <stdlib.h>
+#include <unistd.h>
 
-char *get_next_line(int fd)
+int isnewline(char *str)
+{
+	while (*str)
+	{
+		if (*str == '\n')
+			return 1;
+		str++;
+	}
+	return 0;
+}
+
+t_list *create_node(void *content)
+{
+	t_list *new_node;
+
+	new_node = malloc(sizeof(t_list));
+	if (!new_node)
+		return NULL;
+	new_node->content = content;
+	new_node->next = NULL;
+	return new_node;
+}
+
+void	add_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (!lst || !new)
+		return ;
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	last = *lst;
+	while (last->next)
+		last = last->next;
+	last->next = new;
+}
+
+
+t_list *get_next_line(int fd)
 {
 	t_list *node;
+	t_list *head;
+	static char *str;
+	ssize_t bytes;
 
-	node = malloc(sizeof(t_list));
-	node->content = malloc(BUFFER_SIZE + 1);
-	ssize_t bytes_read = read(fd, node->content, BUFFER_SIZE);
-	char *result = node->content;
-	free(node);
-	result[bytes_read] = '\0';
-	return result;
+	bytes = 1;
+	head = NULL;
+	while (bytes > 0)
+	{
+		str = malloc(BUFFER_SIZE + 1);
+		bytes = read(fd, str, BUFFER_SIZE);
+		str[BUFFER_SIZE] = 0;
+		node = create_node(str);
+		add_back(&head, node);
+		free(str);
+	}
+	return head;
 }
 
 int main()
 {
 	int fd = open("hello.txt", O_RDONLY);
 	char *line;
+	t_list *node;
 
-	printf("%s\n", line = get_next_line(fd));
-	free(line);
-	printf("%s\n", line = get_next_line(fd));
-	free(line);
-	printf("%s\n", line = get_next_line(fd));
-	free(line);
-	printf("%s\n", line = get_next_line(fd));
-	free(line);
-	printf("%s\n", line = get_next_line(fd));
-	free(line);
-	printf("%s\n", line = get_next_line(fd));
-	free(line);
+	node = get_next_line(fd);
+	while (node)
+	{
+		printf("%s\n", (char *)node->content);
+		node = node->next;
+	}
 	close(fd);
 	return 0;
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -18,12 +18,12 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
-#define BUFFER_SIZE 4
+#define BUFFER_SIZE 42
 
 typedef struct s_list {
 	void *content;
 	struct s_list *next;
 } t_list;
 
-char *get_next_line(int fd);
-#endif // !GET_NEXT_LINE_H
+t_list *get_next_line(int fd);
+#endif
