bba948d1596409f1f990d114f7ada899d858a988
Hieu Pham <hieutrungpham91@gmail.com>
2025-05-11 11:16:22 +0300

    feat: more tests

--- ft_printf/test.c
+++ ft_printf/test.c
@@ -1,4 +1,5 @@
 #include "ft_printf.h"
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -19,7 +20,7 @@
     }\
 } while (0)
 
-#define COMPARE_OUTPUT(format, ...) do { \
+#define COMPARE(format, ...) do { \
     char buffer1[1024] = {0}; \
     char buffer2[1024] = {0}; \
     int stdout_backup = dup(fileno(stdout)); \
@@ -30,21 +31,21 @@
         exit(1); \
     } \
     dup2(fileno(output1), fileno(stdout)); \
-	printf(YELLOW"printf: "RESET); \
+    printf(YELLOW"printf: "RESET); \
     printf(format, __VA_ARGS__); \
-	printf("\n"); \
-	printf(YELLOW"ft_printf: "RESET); \
+    printf("\n"); \
+    printf(YELLOW"ft_printf: "RESET); \
     fflush(stdout); \
     fclose(output1); \
     dup2(fileno(output2), fileno(stdout)); \
     ft_printf(format, __VA_ARGS__); \
-	printf("\n"); \
+    printf("\n"); \
     fflush(stdout); \
     fclose(output2); \
     dup2(stdout_backup, fileno(stdout)); \
     close(stdout_backup); \
     TEST(strcmp(buffer1, buffer2) == 0); \
-	printf("\n"); \
+    printf("\n"); \
 } while (0)
 
 int main(void)
@@ -52,23 +53,22 @@ int main(void)
     int count = 0;
     int fail = 0;
     int i = 0;
-	char *str = NULL;
-    // Test basic format
-    COMPARE_OUTPUT("%d %i %p", -1, 32, &i);
-    COMPARE_OUTPUT("%s", str);
-
-    // More tests can be added below:
-    COMPARE_OUTPUT("%d %i %p %c", 123, -456, NULL, 'a'+256);
-    COMPARE_OUTPUT("Hello, %s!", "world");
-    COMPARE_OUTPUT("%x %X", 255, 255);		//testing hex
-    COMPARE_OUTPUT("%d %i %p", -1, 32, &i);  // Testing integer and pointer
-    COMPARE_OUTPUT("%s", "Hello, World!");  // Testing string
-    COMPARE_OUTPUT("%x %X", 255, 255);      // Testing lowercase and uppercase hex
-    COMPARE_OUTPUT("%d", 0);                // Testing zero
-    COMPARE_OUTPUT("%p", NULL);             // Testing null pointer
-    COMPARE_OUTPUT("%u", 123456789);        // Testing unsigned integer
-    COMPARE_OUTPUT("%d%% of hivers are %s", 42, "weeps");        //testing %
-
+    char *str = NULL;
+    /*1*/COMPARE("%d %i %p", -1, 32, &i);
+    /*2*/COMPARE("%s", str);
+    /*3*/COMPARE("null: %d %i %p %c", 123, -456, NULL, 'a'+256);
+    /*4*/COMPARE("%d %i %p", -1, 32, &i);  // Testing integer and pointer
+    /*5*/COMPARE("string: %s", "Hello, World!");  // Testing string
+    /*6*/COMPARE("hex: %x %X", 0xff, -0xff);      // Testing lowercase and uppercase hex
+    /*7*/COMPARE("zero: %d", 0);                // Testing zero
+    /*8*/COMPARE("pointer: %p", NULL);             // Testing null pointer
+    /*9*/COMPARE("unsigned int: %u", 123456789);        // Testing unsigned integer
+    /*10*/COMPARE("Test %%: %d%% of hivers are %s", 42, "weeps");        //testing %
+    /*11*/COMPARE("INT_MIN INT_MAX: %d %i", INT_MIN, INT_MAX);
+    /*12*/COMPARE("LONG_MIN LONG_MAX pointer: %p %p", (void*)LONG_MIN, (void*)LONG_MAX);
+    /*13*/COMPARE("UINT_MAX hexadecimal: %x", UINT_MAX);
+    /*14*/COMPARE("+ULONG_MAX pointer: %p", (void*) +ULONG_MAX);
+    /*15*/COMPARE("-ULONG_MAX pointer: %p", (void*) -ULONG_MAX);
     // Final summary
     if (fail == 0)
         printf(GREEN "\nAll tests passed!" RESET);
