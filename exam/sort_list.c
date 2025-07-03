07682a066af1ac1e85e555eb42c56b47c5aeb594
hieutrpham <hieutrungpham91@gmail.com>
2025-07-03 14:38:55 +0300

    update

--- exam/sort_list.c
+++ exam/sort_list.c
@@ -5,7 +5,7 @@ typedef struct s_list {
 	struct s_list *next;
 } t_list;
 
-t_list *sort_list(t_list *lst, int (*cmp)(int, int))
+t_list *sort_list1(t_list *lst, int (*cmp)(int, int))
 {
 	t_list *head;
 
@@ -23,4 +23,38 @@ t_list *sort_list(t_list *lst, int (*cmp)(int, int))
 			lst = lst->next;
 	}
 	return head;
+}
+
+// i did this in exam
+int issorted(t_list *lst, int* cmp(int, int))
+{
+	while (lst)
+	{
+		if (cmp(lst->data, lst->next->data) == 0)
+			return 0;
+		lst = lst->next;
+	}
+	return 1;
+}
+
+t_list *sort_list(t_list *lst, int *cmp(int, int))
+{
+	t_list *curr;
+
+	curr = lst;
+	while (!issorted(lst, cmp))
+	{
+		curr = lst;
+		while (curr && curr->next)
+		{
+			if (cmp(curr->data, curr->next->data) == 0)
+			{
+				int tmp = curr->data;
+				curr->data = curr->next->data;
+				curr->next->data = tmp;
+			}
+			curr = curr->next;
+		}
+	}
+	return lst;
 }
